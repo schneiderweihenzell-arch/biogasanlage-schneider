a94e418dfaee8f9756a7ffe497213ab9e41d6496
schneiderweihenzell-arch <schneider.weihenzell@web.de>
2026-09-18 07:19:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="bs-radlader-test-v5";
+const CACHE="bs-radlader-test-v6";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg","./default-config.js","./jszip.min.js"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
